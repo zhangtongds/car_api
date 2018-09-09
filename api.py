c605fff0a2cfb1acf0148f32c9c96d8295586349
Tong Zhang <zhangtong.purdue@gmail.com>
2018-09-08 20:13:23 -0700

    Completed vehicle info function

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import pprint
 
 headers = {
     'Content-Type': 'application/json'
@@ -8,14 +10,49 @@ class SmartCarApi(object):
     """
     """
 
-    def __init__(self):
-        self.base_url = "/gmapi.azurewebsites.net/getVehicleInfoService"
+    def __init__(self, ):
+        self.base_url = 'http://gmapi.azurewebsites.net/getVehicleInfoService'
+
+    # def get_vehicle_info_v1(info):
+    #     car_id = info.split('/')[-1]
+    #     self.get_vehicle_info(car_id, "JSON")
 
     def get_vehicle_info(self, car_id, response_type):
-        url_params = {
-            'id': car_id
-            'response_type': response_type
+        """
+        Make request to GM api for vehicle information
+        and return results in Smartcar API format.
+        """
+        search_param = {
+            'id': car_id,
+            'responseType': response_type
         }
-        request_url = self.base_url + '?' + '&'.join(url_params)
-        data = requests.get(request_url, headers = headers)
-        return data
+        data = requests.post(self.base_url, data=json.dumps(search_param), headers=headers)
+        gm_data = data.json()
+        vin = gm_data['data']['vin']['value']
+        color = gm_data['data']['color']['value']
+        door_count = None
+        if gm_data['data']['fourDoorSedan']:
+            door_count = 4
+        if gm_data['data']['twoDoorCoupe']:
+            door_count = 2
+        drive_train = gm_data['data']['driveTrain']['value']
+        smartcar_vehicle_info = {
+                                'vin': vin,
+                                'color': color,
+                                'doorCount': door_count,
+                                'driveTrain': drive_train
+                                }
+        pprint.pprint(smartcar_vehicle_info)
+        return smartcar_vehicle_info
+
+
+    
+
+tester = SmartCarApi()
+
+info = '/vehicles/:id'
+
+tester.get_vehicle_info_v1(info)
+
+tester.get_vehicle_info("1234", "JSON")
+

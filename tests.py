3ca8ee8ca6ddae1595120cc9a931bafd80227cd5
Tong Zhang <zhangtong.purdue@gmail.com>
2018-09-09 15:58:42 -0700

    imporved test coverage to 94%

--- tests.py
+++ tests.py
@@ -12,13 +12,17 @@ class SmartcarTestCase(unittest.TestCase):
         """
         Testing returned keys from vehicle info.
         """
-
+        #Tests for sucess API calls.
         client = server.app.test_client()
         result = client.get('/vehicles/1235')
         self.assertIn(b'vin', result.data)
         self.assertIn(b'color', result.data)
         self.assertIn(b'driveTrain', result.data)
         self.assertIn(b'doorCount', result.data)
+
+        #Tests for calls to GM with invalid id that returns error msgs.
+        error_result = client.get('/vehicles/9999')
+        self.assertIn(b'status', error_result.data)
 
     def test_get_security_status(self):
         """
@@ -30,6 +34,9 @@ class SmartcarTestCase(unittest.TestCase):
         self.assertIn(b'location', result.data)
         self.assertIn(b'locked', result.data)
 
+        error_result = client.get('/vehicles/9999/doors')
+        self.assertIn(b'status', error_result.data)
+
     def test_get_tank_level(self):
         """
         Testing returned keys from fuel level.
@@ -38,6 +45,9 @@ class SmartcarTestCase(unittest.TestCase):
         client = server.app.test_client()
         result = client.get('/vehicles/1235/fuel')
         self.assertIn(b'fuel', result.data)
+
+        error_result = client.get('/vehicles/9999/fuel')
+        self.assertIn(b'status', error_result.data)
 
     def test_get_battery_level(self):
         """
@@ -48,6 +58,9 @@ class SmartcarTestCase(unittest.TestCase):
         result = client.get('/vehicles/1234/battery')
         self.assertIn(b'battery', result.data)
 
+        error_result = client.get('/vehicles/9999/battery')
+        self.assertIn(b'status', error_result.data)
+
 
     def test_change_engine_status(self):
         """
@@ -57,6 +70,11 @@ class SmartcarTestCase(unittest.TestCase):
         client = server.app.test_client()
         result = client.post('/vehicles/1235/engine', data=json.dumps({'action': 'START'}))
         self.assertIn(b'action', result.data)
+        result = client.post('/vehicles/1235/engine', data=json.dumps({'action': 'STOP'}))
+        self.assertIn(b'action', result.data)
+
+        error_result = client.post('/vehicles/9999/engine', data=json.dumps({'action': 'INVALID'}))
+        self.assertIn(b'status', error_result.data)
 
 if __name__ == '__main__':
     unittest.main()
